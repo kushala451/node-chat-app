90ec4da8a8fa9dfedddac37cb0e0aeee7425c06d
kushala <kushala451@gmai.com>
2020-05-15 02:47:26 +0530

    'index.js'

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,18 +1,33 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',() =>{
     it('should generate correct message object',() => {  
         var  from ='raghu';
         var text = 'some message';
-     var createdAt =message.createdAt;
+        //  var createdAt = new Date().getTime();
         var message = generateMessage(from,text);
 
-        expect(message.createdAt).toBe(createdAt);
+        expect(message.createdAt).toBe('number');
         expect(message).toBe({from,text});
-    
+     
 
     });
 
 });
+
+describe('generateLocationMessage',() =>{
+    it('Should generate correct location object', ( ) =>{
+        var from = 'deb';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/search?q=15,19';
+        var message= generateLocationMessage(from, latitude, longitude);
+
+
+        expect(message.createdAt).toBe('number');
+        expect(message).toBe({from,url});
+     
+    })
+})
