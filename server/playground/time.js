fa765ffc7259c08a1ae743041ad76eb21b5e6df8
kushala <kushala451@gmai.com>
2020-05-16 00:44:47 +0530

    'moment'

---
+++ server/playground/time.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+
+
+new Date ().getTime()
+var someTimestamp = moment().valueOf();
+var createdAt =123;
+
+var date = moment(createdAt);
+console.log(date.format('MMM Do, YYYY h:mm a'))
